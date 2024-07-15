a2120f2d546c6387d8a225da843f1ef482cc211e
Yaros2011 <163567543+Yaros2011@users.noreply.github.com>
2024-07-15 15:50:17 +0300

    Add files via upload

---
+++ урок 1/script.js
@@ -0,0 +1,107 @@
+//you are trasgender
+
+/*
+░░░░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░▄██▄░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░▄████▄░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░░░░░
+███▄▄░░░░░░░░░░████████░░░░░░░░░░▄▄▄██
+▀██████▄▄░░░░░░████████░░░░░░▄▄██████▀
+░█████████▄░░░░████████░░░░▄█████████░
+░░▀▀▀▀▀▀▀▀▀▀░░░▀▀▀▀▀▀▀▀░░░▀▀▀▀▀▀▀▀▀▀░░
+░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░
+░░░░▀████████████████████████████▀░░░░
+░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░
+░░░░░░░▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀░░░░░░░
+░░░░░░░░░░░▄▄▄▄▄▄░▄▄░▄▄▄▄▄▄░░░░░░░░░░░
+░░░░░░░░░░░░░░▀▀▀░▀▀░▀▀▀░░░░░░░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░░░░░░██░▀▀░░░░░░██░░░░░░░░░░░░░
+░▄█▀▀██░▄█▀▀██░██░▄█▀▀███░▄█▀▀██░█▀▀█▄
+░█░░░██░█░░░██░██░█░░░███░█░░░██░▀███▄
+░▀█▄▄██░▀█▄▄██░██░▀█▄▄███░▀█▄▄██░█▄▄█▀
+*/
+//Primitives data type
+
+//String
+// const greetings = "/\Ox"
+// console.log(typeof greetings);
+
+//number
+// const someNum = 123;  //+ - / * %
+// const num = 125.2;
+// console.log(typeof someNum);
+// console.log(typeof num);
+
+
+//boolean
+// const boolTrue = true;
+// const boolFalse = false;
+// console.log(typeof boolTrue);
+
+//null
+// let nullPointer = null;
+// console.log(nullPointer);
+
+//underfined
+// let unde;
+// console.log(unde);
+
+
+
+//object
+
+// let obj = {
+//    name: "ПОГРАНИЧНИК ПИСЮН",
+//    hobby: "modelizm"
+// };
+
+// console.log(obj);
+// obj.age = 13;
+// console.log(obj);
+// console.log(obj.age);
+
+//array
+// let arr = [1488, 666, 999.3, true, "Hyi", 193.367];
+// console.log(arr);
+// console.log(arr[0]);
+
+
+// let firstVar = prompt("скоко тротила скидиваем гандон?:");
+// console.log(typeof firstVar);
+// let secondVar = prompt("мощность");
+
+// firstVar = parseInt(firstVar);
+// secondVar = parseInt(secondVar);
+
+// let result = firstVar*secondVar;
+
+// if(result>= 250000){
+//     alert ("А НЕ ДОХУЯ ЛИ? ТИ СЕРВАК УЕБАЛ!")
+// } else{
+//     alert("СЕРВАК ВИДЕРЖАЛ. ИДИ НАХУЙ.")
+// }
+
+// let age = prompt("укажи возраст гандон");
+// age = parseInt(age);
+
+// if(age<12){
+//     alert("ти еще обоссанец");
+// } else if(age<18){
+//     alert("ти еще писюн");
+// } else if(age<100){
+//     alert("ти уже скуф...");
+// } else {
+//     alert("ти хто");
+// }
+
+let name = prompt("укажи ник");
+alert("прив, " +name);
+
+console.log(1+1+1);
+console.log(1+"1"+1);
+console.log("1"+1+1);
+console.log(1+1+"1");
+
+
+
